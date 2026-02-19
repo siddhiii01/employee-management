3375fcff2d525f954c3c3f553d6d4b3590155ac9
siddhiii01 <siddhiigupta15@gmail.com>
2026-02-19 18:42:40 +0530

    added Employee model with schema

---
+++ backend/models/Employee.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const employeeSchema = new mongoose.Schema(
+    {
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        dateOfBirth: {
+            type: Date,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+            enum: ['Male', 'Female', 'Other']
+        },
+        department: {
+            type: String,
+            required: true,
+        },
+        designation: {
+            type: String,
+            required: true
+        },
+        photo: {
+            type: String
+        }
+    }, { timestamps: true}
+);
+
+export const Employee = mongoose.model("Employee", employeeSchema);
