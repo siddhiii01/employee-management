0c716250814ce4f78cf8720d9715ae441aea175b
siddhiii01 <siddhiigupta15@gmail.com>
2026-02-19 22:33:34 +0530

    frontend setup with routing, private route, and api service

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,39 @@
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+
+
+const PrivateRoute = ({children}) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login"></Navigate>
+}
 export const App = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/login" 
+          element = {
+            <Login />
+          }
+        />
+        <Route 
+          path="/dashboard"
+          element= {
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route 
+          path="*"
+          element={
+            <Navigate to="/login" />
+          }
+        />
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+//creating axios instance
+export const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
+
+//request interceptor -> runs before sending any request to backend
+API.interceptors.request.use((req) => {
+    console.log('req: ', req)
+    const token = localStorage.getItem('token');
+    if(token) req.headers.Authorization = `${token}`;
+    return req;
+});
+
+export const loginUser = (data) => API.post('/auth/login', data);
+export const createEmployee = (data) => API.post('/employees', data);
+export const getEmployees = (params) => API.get('/employees', {params});
+
