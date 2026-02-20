5a9753cdc9e8f0c141f118f98be0c207f6bf546d
siddhiii01 <siddhiigupta15@gmail.com>
2026-02-20 16:29:06 +0530

    added Client URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "https://idms-nine.vercel.app", credentials: true }))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use('/uploads', express.static('uploads'))
